37bd19afaca61843cf7c49dd0e8593cd0f1b772a
lddv <leodias.vasconcelos@yahoo.com.br>
2016-12-26 11:40:53 -0200

    Using Express and Nodemon.

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-// import the http module
-var http = require('http');
+// // import the http module
+// var http = require('http');
+//
+// // handle receiving requests and returning responses
+// function handleRequests(req, res) {
+//   // return string
+//   res.end('Hello World!');
+// }
+//
+// // create the server
+// var server = http.createServer(handleRequests);
+//
+// // start server and listen on port X
+// server.listen(8089, function() {
+//   console.log('Listening on port 8089');
+// });
 
-// handle sending requests and returning responses
-function handleRequests(req, res) {
-  // return string
-  res.end('Hello World!');
-}
 
-// create the server
-var server = http.createServer(handleRequests);
+/////////////////// EXPRESS ///////////////////
 
-// start server and listen on port X
-server.listen(8080, function() {
-  console.log('Listening on port 8080');
+var express = require('express');
+var app = express();
+var port = 8089;
+
+// start the server
+app.listen(port, function() {
+  console.log('app started');
+});
+
+// route our app
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
