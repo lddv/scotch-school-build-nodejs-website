73ad27ff6d90acf086272205463d465e798a6ad2
lddv <leodias.vasconcelos@yahoo.com.br>
2016-12-27 16:01:17 -0200

    Passing Data to Views

--- app/routes.js
+++ app/routes.js
@@ -10,12 +10,19 @@ module.exports = router;
 
 // route our homepage
 router.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/home');
 });
 
 // route our about page
 router.get('/about', function(req, res) {
-  res.render('pages/about');
+  var users = [
+    { name: 'Holly', email: 'holly@scotch.io', avatar: 'http://placekitten.com/300/300' },
+    { name: 'Chris', email: 'chris@scotch.io', avatar: 'http://placekitten.com/400/400' },
+    { name: 'Ado', email: 'ado@scotch.io', avatar: 'http://placekitten.com/500/500' },
+    { name: 'Nick', email: 'nick@scotch.io', avatar: 'http://placekitten.com/700/700' }
+  ];
+
+  res.render('pages/about', { users: users });
 });
 
 router.get('/contact', function(req, res) {
