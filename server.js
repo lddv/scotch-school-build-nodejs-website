3971cb561891a61481422c9020b8e971f4342203
lddv <leodias.vasconcelos@yahoo.com.br>
2016-12-28 11:14:33 -0200

    Form Routing

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 // require our dependencies
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
 var app = express();
 var port = 8089;
 
 // use ejs and express Layouts
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+// use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // route our app
 var router = require('./app/routes');
